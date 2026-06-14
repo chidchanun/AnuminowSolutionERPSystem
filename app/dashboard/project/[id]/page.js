cdc3cc1b3a01b6d8347cacafe3e0d5225128ff0f
bankbank123 <chidchanun.bank@mail.kmutt.ac.th>
2026-06-15 00:32:27 +0700

    11

--- app/dashboard/project/[id]/page.js
+++ app/dashboard/project/[id]/page.js
@@ -294,7 +294,7 @@ export default async function DetailProjectPage({ params }) {
                                             width={48}
                                             height={48}
                                             sizes="48px"
-                                            className='rounded-full'
+                                            className='rounded-full object-fill'
                                         /> :
                                         member.full_name?.charAt(0)
                                     }
